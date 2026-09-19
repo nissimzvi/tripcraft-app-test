06ddb26dcefa1c15239328fe5f2b70ac68f3867f
nissimzvi <nzvi576@gmail.com>
2026-09-19 12:10:49 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const VERSION='tripcraft-v66-network-only';
-self.addEventListener('install',()=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil((async()=>{for(const k of await caches.keys())await caches.delete(k);await self.clients.claim();})()));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request)));
+const VERSION='tripcraft-v72';
+const STATIC=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(VERSION).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==VERSION)await caches.delete(k);await self.clients.claim();})()));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith((async()=>{try{const r=await fetch(e.request);if(r&&r.ok&&new URL(e.request.url).origin===self.location.origin){const c=await caches.open(VERSION);c.put(e.request,r.clone()).catch(()=>{});}return r;}catch(err){return (await caches.match(e.request))||(await caches.match('./index.html'))||Response.error();}})());});
